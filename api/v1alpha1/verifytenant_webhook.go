fefb9a6b430047a2e81d10dddbd6fa952caccb78
Lachlan Gleeson <lgleeson@au1.ibm.com>
2021-07-14 17:04:53 +1000

    successfully installed controller into openshift

---
+++ api/v1alpha1/verifytenant_webhook.go
@@ -0,0 +1,108 @@
+/*
+Copyright 2021 Lachlan Gleeson.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	"errors"
+	"fmt"
+	"net/mail"
+	"net/url"
+)
+
+// log is for logging in this package.
+var verifytenantlog = logf.Log.WithName("verifytenant-resource")
+
+func (r *VerifyTenant) SetupWebhookWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewWebhookManagedBy(mgr).
+		For(r).
+		Complete()
+}
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+
+// TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
+//+kubebuilder:webhook:path=/validate-ibm-com-v1alpha1-verifytenant,mutating=false,failurePolicy=fail,sideEffects=None,groups=ibm.com,resources=verifytenants,verbs=create;update,versions=v1alpha1,name=vverifytenant.kb.io,admissionReviewVersions={v1,v1beta1}
+
+var _ webhook.Validator = &VerifyTenant{}
+
+// ValidateCreate implements webhook.Validator so a webhook will be registered for the type
+func (r *VerifyTenant) ValidateCreate() error {
+	verifytenantlog.Info("validate create", "name", r.Name)
+
+	// TODO(user): fill in your validation logic upon object creation.
+	err := r.validateDomain(r.Spec.SuperTenant)
+	if err != nil {
+		return err
+	}
+	err = r.validateDomain(r.Spec.Tenant)
+	if err != nil {
+		return err
+	}
+	err = r.validateDomain(r.Spec.Contact)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
+func (r *VerifyTenant) ValidateUpdate(old runtime.Object) error {
+	verifytenantlog.Info("validate update", "name", r.Name)
+
+	// TODO(user): fill in your validation logic upon object update.
+	err := r.validateDomain(r.Spec.SuperTenant)
+	if err != nil {
+		return err
+	}
+	err = r.validateDomain(r.Spec.Tenant)
+	if err != nil {
+		return err
+	}
+	err = r.validateDomain(r.Spec.Contact)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// ValidateDelete implements webhook.Validator so a webhook will be registered for the type
+func (r *VerifyTenant) ValidateDelete() error {
+	verifytenantlog.Info("validate delete", "name", r.Name)
+
+	// TODO(user): fill in your validation logic upon object deletion.
+	return nil
+}
+
+//Validate that string represents a valid domain name
+func (r *VerifyTenant) validateDomain(domain string) error {
+
+	u, err := url.ParseRequestURI(domain)
+	if err != nil || u.Scheme == "" || u.Host == "" {
+		return errors.New(fmt.Sprintf("Invalid hostname specified: %v", domain))
+	}
+	return nil
+}
+
+func (r *VerifyTenant) validateEmail(address string) error {
+	_, err := mail.ParseAddress(address)
+	return err
+}
---
+++ controllers/verifytenant_controller.go
@@ -0,0 +1,214 @@
+/*
+Copyright 2021 Lachlan Gleeson.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	ibmv1alpha1 "github.com/IBM-Security/verify-operator/api/v1alpha1"
+)
+
+// VerifyTenantReconciler reconciles a VerifyTenant object
+type VerifyTenantReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+// JSON Response from Verify for tenant creation
+type VerifyCreateResponse struct {
+	tenant        string
+	client_id     string
+	client_secret string
+}
+
+var _log = logf.Log.WithName("verify_tenant_controller")
+
+//+kubebuilder:rbac:groups=ibm.com,resources=verifytenants,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=ibm.com,resources=verifytenants/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=ibm.com,resources=verifytenants/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the VerifyTenant object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
+func (r *VerifyTenantReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_log.Info("Reconcile VerifyTenant entry")
+
+	// your logic here
+	//Get our custom resource
+	instance := &ibmv1alpha1.VerifyTenant{}
+	err := r.Client.Get(context.TODO(), req.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return reconcile.Result{}, nil
+		}
+		return reconcile.Result{}, err
+	}
+
+	tenantParams := url.Values{}
+	tenantParams.Set("requestedHostname", instance.Spec.Tenant)
+	tenantParams.Set("companyName", instance.Spec.Company)
+	tenantParams.Set("contactEmail", instance.Spec.Contact)
+	tenantParams.Set("wait", "true")
+	tenantParams.Set("isTrial", "false")
+
+	//Check if r.Status contains a version
+	if instance.Status.Version > 0 { //If it does then we need to check if the secret needs to be updated
+		if instance.Status.Version != instance.Spec.Version {
+			//Client secret needs to be updated
+			jsonParams, err := r.regenerateSecretJson(instance)
+			if err != nil {
+				return reconcile.Result{}, err
+			}
+			response, err := r.makeRequest(instance.Spec.SuperTenant, "PUT", instance.Spec.Integration,
+							tenantParams.Encode(), jsonParams)
+			if err != nil {
+				return reconcile.Result{}, err
+			}
+			err = r.updateSecret(*response, instance)
+			if err != nil {
+				return reconcile.Result{}, err
+			}
+			_log.Info("Client secret regenrated")
+
+		} else {
+			_log.Info("Version is correct, nothing to do")
+		}
+	} else { //Otherwise create the Verify tenant and store the generated client id and secret
+		jsonParams := []byte(`[{"key":"oauthClient",
+				"value": "{\"entitlements\":[\"manageOidcGrants\",\"manageApiClients\",\"manageUsers\"]}"}]`)
+		response, err := r.makeRequest(instance.Spec.SuperTenant, "POST", instance.Spec.Integration, 
+							tenantParams.Encode(), jsonParams)
+		if err != nil {
+			return reconcile.Result{}, err
+		}
+		secret := r.createSecret(*response, instance)
+		err = r.Client.Create(context.TODO(), secret)
+		if err != nil {
+			return reconcile.Result{}, err
+		}
+	}
+	_log.Info("Reconcile VerifyTenant exit")
+	return ctrl.Result{}, nil
+}
+
+func (r *VerifyTenantReconciler) regenerateSecretJson(cr *ibmv1alpha1.VerifyTenant) ([]byte, error) {
+
+	tenantSecret := &corev1.Secret{}
+	err := r.Client.Get(context.TODO(), types.NamespacedName{Name: cr.Name, Namespace: cr.Namespace},
+		tenantSecret)
+	if err != nil {
+		return nil, err
+	}
+	if err := controllerutil.SetControllerReference(cr, tenantSecret, r.Scheme); err != nil {
+		return nil, err
+	}
+
+	// Read client id
+	clientId := string(tenantSecret.Data["client_id"])
+	if clientId == "" {
+		return nil, errors.NewNotFound(corev1.Resource("secret"), "Client id not found")
+	}
+	//Can now create the JSON to regenerate the client secret
+	jsonData := []byte(`[{"key": "client_id", "value": "` + clientId + `"},
+		{"key": "oauthClient", "value": "{\"entitlements\":[\"manageOidcGrants\",\"manageApiClients\",\"manageUsers\"]}"}]`)
+	return jsonData, nil
+}
+
+func (r *VerifyTenantReconciler) makeRequest(domain string, method string, integration string, queryParams string, 
+            jsonParams []byte) (*http.Response, error) {
+    uri := fmt.Sprinf("https://%s/tms/v1.0/integration/tenant/%s", domain, integration)
+	request, err := http.NewRequest(method, uri, nil)
+	if err != nil {
+		return nil, err
+	}
+	if queryParams != "" {
+		request.URL.RawQuery = queryParams
+	}
+	if jsonParams != nil {
+		request.Body = ioutil.NopCloser(bytes.NewBuffer(jsonParams))
+	}
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	request.Header.Set("Accept", "application/json")
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	} else {
+		defer response.Body.Close()
+		return response, nil
+	}
+}
+
+func (r *VerifyTenantReconciler) createSecret(response http.Response, cr *ibmv1alpha1.VerifyTenant) *corev1.Secret {
+	labels := map[string]string{
+		"app": cr.Name,
+	}
+	jsonData := &VerifyCreateResponse{}
+	json.NewDecoder(response.Body).Decode(&jsonData)
+	return &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      cr.Name + "-secret",
+			Namespace: cr.Namespace,
+			Labels:    labels,
+		},
+		Type: "generic",
+		StringData: map[string]string{
+			"tenant":        jsonData.tenant,
+			"client_id":     jsonData.client_id,
+			"client_secret": jsonData.client_secret,
+		},
+	}
+}
+
+func (r *VerifyTenantReconciler) updateSecret(response http.Response, cr *ibmv1alpha1.VerifyTenant) error {
+	//TODO
+	return nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *VerifyTenantReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&ibmv1alpha1.VerifyTenant{}).
+		WithOptions(controller.Options{MaxConcurrentReconciles: 2}).
+		Owns(&corev1.Secret{}).
+		Complete(r)
+}
