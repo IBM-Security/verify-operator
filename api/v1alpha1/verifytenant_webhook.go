503f863a21da9f7047523a4ea60d4c251013fa0f
Lachlan Gleeson <lgleeson@au1.ibm.com>
2021-07-20 12:36:13 +1000

    FINALLY GOT WEBHOOK WORKINGgit status!

--- api/v1alpha1/verifytenant_webhook.go
+++ api/v1alpha1/verifytenant_webhook.go
@@ -40,7 +40,7 @@ func (r *VerifyTenant) SetupWebhookWithManager(mgr ctrl.Manager) error {
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 
 // TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
-//+kubebuilder:webhook:path=/validate-ibm-com-v1alpha1-verifytenant,mutating=false,failurePolicy=fail,sideEffects=None,groups=ibm.com,resources=verifytenants,verbs=create;update,versions=v1alpha1,name=vverifytenant.kb.io,admissionReviewVersions={v1,v1beta1}
+//+kubebuilder:webhook:path=/validate-ibm-com-v1alpha1-verifytenant,mutating=false,failurePolicy=fail,sideEffects=None,groups=ibm.com,resources=verifytenants,verbs=create;update,versions=v1alpha1,name=vverifytenant.kb.io,admissionReviewVersions={v1alpha1,v1beta1}
 
 var _ webhook.Validator = &VerifyTenant{}
 
